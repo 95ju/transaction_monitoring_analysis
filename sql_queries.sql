08fc7be24026ab0c4cf38b4f6f58d7a4dd050191
95ju <frutos_julia1@yahoo.com>
2026-05-25 20:33:04 +0200

    Update sql_queries.sql

--- sql_queries.sql
+++ sql_queries.sql
@@ -1,6 +1,5 @@
-Transaction_analysis_queries.sql
 
-—Display first 5 transactions
+—-Display first 5 transactions
 SELECT *
 FROM transactions
 LIMIT 5;
@@ -9,7 +8,7 @@ LIMIT 5;
 SELECT COUNT(*) 
 FROM transactions;
 
-—Revenue by country
+—-Revenue by country
 SELECT COUNTRY,
 SUM(numberOfitemsPurchased*costperitem) AS total_revenue
 FROM transactions
@@ -20,12 +19,12 @@ Revenue Insights:
 United Kingdom generates the highest revenue by far.
 European countries dominate total sales revenue.
 
-frutos=# SELECT itemdescription,
-frutos-# SUM(numberofitemspurchased)AS total_items_sold
-frutos-# FROM transactions
-frutos-# GROUP BY itemdescription
-frutos-# ORDER BY total_items_sold DESC
-frutos-# LIMIT 10;
+SELECT itemdescription,
+SUM(numberofitemspurchased)AS total_items_sold
+FROM transactions
+GROUP BY itemdescription
+ORDER BY total_items_sold DESC
+LIMIT 10;
 
 Business Insight:
 The most sold product is
@@ -34,12 +33,12 @@ World war 2 gliders ASST DESIGNS
 Several decorative and gift-related items dominate sales volume,
 Suggesting strong demand for home and novelty products.
 
-frutos=# SELECT country,
-frutos-# COUNT(*) AS total_transactions
-frutos-# FROM transactions
-frutos-# GROUP BY country
-frutos-# ORDER BY total_transactions DESC
-frutos-# LIMIT 10;
+SELECT country,
+COUNT(*) AS total_transactions
+FROM transactions
+GROUP BY country
+ORDER BY total_transactions DESC
+LIMIT 10;
 
 Business Insight:
 The United Kingdom generates the highest transaction volume by far.
@@ -47,60 +46,60 @@ The Netherlands generate high revenue with less transactions  4742, which may in
 
 
 Customer behavior Insights:
-frutos=# SELECT country,
-frutos-# SUM(numberofitemspurchased*costperitem)/ COUNT(*) AS avg_revenue_per_transaction
-frutos-# FROM transactions
-frutos-# GROUP BY country
-frutos-# ORDER BY avg_revenue_per_transaction DESC
-frutos-# LIMIT 10;
+SELECT country,
+SUM(numberofitemspurchased*costperitem)/ COUNT(*) AS avg_revenue_per_transaction
+FROM transactions
+GROUP BY country
+ORDER BY avg_revenue_per_transaction DESC
+LIMIT 10;
 
 Netherlands has a higher average transaction value
 This could indicate:
--higher-priced products
+-higher priced products
 -Bulk purchases
 -larger basket sizes
 -business/commercial customers
 
-frutos=# SELECT userid,
-frutos-# SUM(numberofitemspurchased*costperitem) AS total_customer_revenue
-frutos-# FROM transactions
-frutos-# GROUP BY userid
-frutos-# ORDER BY total_customer_revenue DESC
-frutos-# LIMIT 10;
+SELECT userid,
+ SUM(numberofitemspurchased*costperitem) AS total_customer_revenue
+FROM transactions
+GROUP BY userid
+ORDER BY total_customer_revenue DESC
+LIMIT 10;
 
 Business insight:
 A small number of customers generate a disproportionately high share of total revenue.
 This may indicate the presence of high-value commercial customers or bulk purchasing behavior.
 One customer ID(-1) appears among the top revenue generators, which may indicate missing customer information, anonymous transactions, or a potential data quality issue.
 
-frutos=# SELECT userid,
-frutos-# itemdescription,
-frutos-# numberofitemspurchased,
-frutos-# costperitem,
-frutos-# (numberofitemspurchased*costperitem)AS transaction_value
-frutos-# FROM transactions
-frutos-# ORDER BY transaction_value DESC
-frutos-# LIMIT 10;
+SELECT userid,
+itemdescription,
+numberofitemspurchased,
+costperitem,
+(numberofitemspurchased*costperitem)AS transaction_value
+FROM transactions
+ORDER BY transaction_value DESC
+ LIMIT 10;
 
 Several transactions contain unusually high transaction values, particularly for low-value retail products such as lamps.
 This may indicate data quality issues, pricing anomalies, or erroneous transaction records.
 
-frutos=# WITH ranked_customers AS(
-frutos(# SELECT country,
-frutos(# userid,
-frutos(# SUM(numberofitemspurchased*costperitem) AS total_revenue,
-frutos(# RANK() OVER(
-frutos(# PARTITION BY country
-frutos(# ORDER BY SUM(numberofitemspurchased*costperitem) DESC) AS revenue_rank
-frutos(# FROM transactions
-frutos(# GROUP BY country, userid)
-frutos-# SELECT country,
-frutos-# userid,
-frutos-# total_revenue,
-frutos-# revenue_rank
-frutos-# FROM ranked_customers
-frutos-# WHERE revenue_rank <=3
-frutos-# ORDER BY country, revenue_rank;
+WITH ranked_customers AS(
+SELECT country,
+userid,
+SUM(numberofitemspurchased*costperitem) AS total_revenue,
+RANK() OVER(
+PARTITION BY country
+ORDER BY SUM(numberofitemspurchased*costperitem) DESC) AS revenue_rank
+FROM transactions
+GROUP BY country, userid)
+SELECT country,
+userid,
+total_revenue,
+revenue_rank
+FROM ranked_customers
+WHERE revenue_rank <=3
+ORDER BY country, revenue_rank;
 
 For each country show the top 3 customers by total revenue
 
@@ -112,170 +111,170 @@ Australia            | 260715 |     1026822.9000000007 |            1
 
 Some countries contain unusually high-revenue customers, which may represent commercial buyers, bulk purchasing activity, or potential anomalies requiring further investigation.
 
-frutos=# SELECT country,
-frutos-# AVG(numberofitemspurchased) AS avg_quantity_purchased
-frutos-# FROM transactions
-frutos-# GROUP BY country
-frutos-# ORDER BY avg_quantity_purchased DESC
-frutos-# LIMIT 10;
+SELECT country,
+AVG(numberofitemspurchased) AS avg_quantity_purchased
+FROM transactions
+GROUP BY country
+ORDER BY avg_quantity_purchased DESC
+LIMIT 10;
 
 Counties such as the Netherlands, Sweden, and Japan show significantly higher average purchase quantities, which may indicate bulk purchasing behavior or a stronger presence of commercial customers
 
-frutos=# SELECT itemdescription,
-frutos-# SUM (numberofitemspurchased*costperitem) AS total_product_revenue
-frutos-# FROM transactions
-frutos-# GROUP BY itemdescription
-frutos-# ORDER BY total_product_revenue DESC
-frutos-# LIMIT 10;
+SELECT itemdescription,
+SUM (numberofitemspurchased*costperitem) AS total_product_revenue
+FROM transactions
+GROUP BY itemdescription
+ORDER BY total_product_revenue DESC
+LIMIT 10;
 
 Operational Insights:
 A small number of products generate disproportionally high revenue, with certain items such as RETROSPOT LAMP showing unusually large transaction values that may indicate  pricing anomalies or data quality issues
 
 Which countries generate the highest average revenue per custo,er+
 
-frutos=# SELECT country,
-frutos-# SUM(numberofitemspurchased*costperitem)/COUNT(DISTINCT userid) AS avg_revenue_per_customer
-frutos-# FROM transactions
-frutos-# GROUP BY country
-frutos-# ORDER BY avg_revenue_per_customer DESC
-frutos-# LIMIT 10;
+SELECT country,
+SUM(numberofitemspurchased*costperitem)/COUNT(DISTINCT userid) AS avg_revenue_per_customer
+FROM transactions
+GROUP BY country
+ORDER BY avg_revenue_per_customer DESC
+LIMIT 10;
 
 Countries such as EIRE and the Netherlands generate significantly higher average rev by customer, suggesting the presence of high-value customers or stronger purchasing behavior in these markets.
 
-frutos=# WITH customer_revenue AS(
-frutos(# SELECT country,
-frutos(# userid,
-frutos(# SUM(numberofitemspurchased*costperitem) AS total_revenue
-frutos(# FROM transactions
-frutos(# GROUP BY country,userid
+WITH customer_revenue AS(
+SELECT country,
+userid,
+SUM(numberofitemspurchased*costperitem) AS total_revenue
+FROM transactions
+GROUP BY country,userid
 frutos(# )
-frutos-# SELECT country,
-frutos-# userid,
-frutos-# total_revenue,
-frutos-# AVG(total_revenue)OVER(
-frutos(# PARTITION BY country
-frutos(# ) AS country_avg_revenue
-frutos-# FROM customer_revenue
-frutos-#  ORDER BY total_revenue DESC
-frutos-# LIMIT 20;
+SELECT country,
+userid,
+total_revenue,
+AVG(total_revenue)OVER(
+PARTITION BY country
+AS country_avg_revenue
+FROM customer_revenue
+ORDER BY total_revenue DESC
+LIMIT 20;
 
 Some customers generate revenue far above their country average, indicating strong customer concentration and potential high-value or anomalous purchasing behavior
 
 Risk/anomaly Insight:
 
-frutos=# SELECT userid,
-frutos-# country,
-frutos-# itemdescription,
-frutos-# numberofitemspurchased,
-frutos-# costperitem,
-frutos-# (numberofitemspurchased*costperitem) AS transaction_value
-frutos-# FROM transactions
-frutos-# WHERE (numberofitemspurchased*costperitem)>100000
-frutos-# ORDER BY transaction_value DESC
-frutos-# LIMIT 20;
+SELECT userid,
+country,
+ itemdescription,
+ numberofitemspurchased,
+costperitem,
+ (numberofitemspurchased*costperitem) AS transaction_value
+ FROM transactions
+WHERE (numberofitemspurchased*costperitem)>100000
+ORDER BY transaction_value DESC
+LIMIT 20;
 
 
 Several transactions contain unusually high purchase quantities and transaction values, particularly in the United Kingdom which might indicate bulk purchases, data quality issues or potentially anomalies requiring further investigation
 
 
 
-frutos=# CREATE TABLE customers(
-frutos(# userid INT,
-frutos(# customer_segment TEXT
-frutos(# );
+# CREATE TABLE customers(
+(# userid INT,
+(# customer_segment TEXT
+(# );
 CREATE TABLE
-frutos=# INSERT INTO customers VALUES
-frutos-# (278166,'Retail'),
-frutos-# (337701,'Corporate'),
-frutos-# (267099,'Small Business'),
-frutos-# (380478,'VIP'),
-frutos-# (288687,'Corporate');
+INSERT INTO customers VALUES
+(278166,'Retail'),
+(337701,'Corporate'),
+(267099,'Small Business'),
+ (380478,'VIP'),
+(288687,'Corporate');
 INSERT 0 5
 
 
-frutos=# SELECT t.userid,
-frutos-#  t.country,
-frutos-# c.customer_segment,
-frutos-# SUM(t.numberofitemspurchased*t.costperitem) AS total_revenue
-frutos-# FROM transactions t
-frutos-# INNER JOIN customers c
-frutos-# on t.userid=c.userid
-frutos-# GROUP BY t.userid,t.country,c.customer_segment
-frutos-# ORDER BY total_revenue DESC;
+SELECT t.userid,
+ t.country,
+c.customer_segment,
+SUM(t.numberofitemspurchased*t.costperitem) AS total_revenue
+ FROM transactions t
+INNER JOIN customers c
+on t.userid=c.userid
+GROUP BY t.userid,t.country,c.customer_segment
+ORDER BY total_revenue DESC;
 
 
 Corporate customers generate significantly higher revenue compared to other business segments, suggesting that business-oriented clients contribute a major sales of total shares
 
 
-frutos=# SELECT c.customer_segment,
-frutos-# COUNT(DISTINCT t.userid) AS total_customers,
-frutos-# SUM(t.numberofitemspurchased*t.costperitem)AS total_revenue,
-frutos-# AVG(t.numberofitemspurchased*t.costperitem)AS avg_transaction_value
-frutos-#  FROM transactions t
-frutos-# INNER JOIN customers c
-frutos-# on t.userid=c.userid
-frutos-# GROUP BY c.customer_segment
-frutos-# ORDER BY total_revenue DESC;
+SELECT c.customer_segment,
+COUNT(DISTINCT t.userid) AS total_customers,
+SUM(t.numberofitemspurchased*t.costperitem)AS total_revenue,
+AVG(t.numberofitemspurchased*t.costperitem)AS avg_transaction_value
+ FROM transactions t
+INNER JOIN customers c
+on t.userid=c.userid
+GROUP BY c.customer_segment
+ORDER BY total_revenue DESC;
 
 Corporate customers represent a very small customer group but generate disproportionately high revenue, suggesting that a limited number of business clients contribute heavily to total sales.
 
-frutos=# SELECT t.userid,
-frutos-# t.country,
-frutos-# c.customer_segment
-frutos-# FROM transactions t
-frutos-# LEFT JOIN customers c
-frutos-# ON t.userid=c.userid
-frutos-# WHERE c.customer_segment IS NULL
-frutos-# LIMIT 20;
+SELECT t.userid,
+t.country,
+c.customer_segment
+ FROM transactions t
+LEFT JOIN customers c
+ON t.userid=c.userid
+WHERE c.customer_segment IS NULL
+ LIMIT 20;
 
 A large number of transaction records do not have corresponding customer information, which may indicate incomplete customer data, on boarding gaps, or missing CRM mappings
 
-frutos=# SELECT userid,
-frutos-# country,
-frutos-# (numberofitemspurchased*costperitem) AS transaction_value,
-frutos-# CASE
-frutos-# WHEN (numberofitemspurchased*costperitem)<100 THEN'Low value'
-frutos-# WHEN (numberofitemspurchased*costperitem) BETWEEN 100 AND 1000 THEN 'Medium Value'
-frutos-# ELSE 'High Value'
-frutos-# END AS transaction_category
-frutos-# FROM transactions
-frutos-# ORDER BY transaction_value ASC
-frutos-# LIMIT 50;
+SELECT userid,
+country,
+(numberofitemspurchased*costperitem) AS transaction_value,
+CASE
+WHEN (numberofitemspurchased*costperitem)<100 THEN'Low value'
+WHEN (numberofitemspurchased*costperitem) BETWEEN 100 AND 1000 THEN 'Medium Value'
+ELSE 'High Value'
+END AS transaction_category
+FROM transactions
+ORDER BY transaction_value ASC
+LIMIT 50;
 
 A significant number of transactions have negative values, suggesting the presence of refunds or cancelled orders within the dataset
 
-frutos=# SELECT c.customer_segment,
-frutos-# CASE
-frutos-# WHEN (t.numberofitemspurchased*t.costperitem)>10000 THEN 'High Risk'
-frutos-# WHEN (t.numberofitemspurchased*t.costperitem) BETWEEN 1000 AND 10000 THEN 'Medium Risk'
-frutos-# ELSE 'Low Risk'
-frutos-# END AS risk_category,
-frutos-# COUNT(*) AS total_transactions,
-frutos-# SUM(t.numberofitemspurchased*t.costperitem) AS total_revenue
-frutos-# FROM transactions t
-frutos-# INNER JOIN customers c
-frutos-# on t.userid=c.userid
-frutos-# GROUP BY c.customer_segment, risk_category
-frutos-# ORDER BY total_revenue DESC;
+SELECT c.customer_segment,
+CASE
+WHEN (t.numberofitemspurchased*t.costperitem)>10000 THEN 'High Risk'
+ WHEN (t.numberofitemspurchased*t.costperitem) BETWEEN 1000 AND 10000 THEN 'Medium Risk'
+ELSE 'Low Risk'
+END AS risk_category,
+COUNT(*) AS total_transactions,
+SUM(t.numberofitemspurchased*t.costperitem) AS total_revenue
+FROM transactions t
+INNER JOIN customers c
+on t.userid=c.userid
+GROUP BY c.customer_segment, risk_category
+ORDER BY total_revenue DESC;
 
 Corporate customers are responsible for the only high-risk transactions identified in the dataset, indicating that a small subset of business-oriented clients generates exceptionally large transaction values
 
 
-frutos=# WITH transaction_analysis AS(
-frutos(# SELECT country,
-frutos(# userid,
-frutos(# (numberofitemspurchased*costperitem) AS transaction_value,
-frutos(# AVG (numberofitemspurchased*costperitem)
-frutos(# OVER (PARTITION BY country) AS country_avg_transaction
-frutos(# FROM transactions)
-frutos-# SELECT country,
-frutos-# userid,
-frutos-# transaction_value,
-frutos-# country_avg_transaction,
-frutos-# transaction_value-country_avg_transaction AS difference_from_avg
-frutos-# FROM transaction_analysis
-frutos-# ORDER BY difference_from_avg DESC
-frutos-# LIMIT 20;
+WITH transaction_analysis AS(
+SELECT country,
+userid,
+(numberofitemspurchased*costperitem) AS transaction_value,
+AVG (numberofitemspurchased*costperitem)
+OVER (PARTITION BY country) AS country_avg_transaction
+FROM transactions)
+SELECT country,
+userid,
+transaction_value,
+ country_avg_transaction,
+transaction_value-country_avg_transaction AS difference_from_avg
+FROM transaction_analysis
+ORDER BY difference_from_avg DESC
+LIMIT 20;
 
 
 Several transactions in the United Kingdom deviate massively from the country’s average transaction value, suggesting the presence of significant outliers, bulk purchases, or potentially anomalous transaction activity
